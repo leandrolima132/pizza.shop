07060711b0a7df007280f2af0adeff656d66d46a
Leandro Ferreira <leandrolima_ferreira@hotmail.com>
2024-08-23 16:44:53 -0300

    feat: create theme toggle component

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,17 @@ import { RouterProvider } from "react-router-dom"
 import { router } from "./routers"
 import { Helmet, HelmetProvider } from "react-helmet-async"
 import { Toaster } from "sonner"
+import { ThemeProvider } from "./components/theme-provider"
 
 function App() {
 
   return (
     <HelmetProvider>
-      <Helmet titleTemplate="%s | Pizza.shop"/>
-      <RouterProvider router={router} />
-      <Toaster richColors/>
+      <ThemeProvider defaultTheme="system" storageKey="@pizza-shop:theme">
+        <Helmet titleTemplate="%s | Pizza.shop"/>
+        <RouterProvider router={router} />
+        <Toaster richColors/>
+      </ThemeProvider>
     </HelmetProvider>
   )
 }
