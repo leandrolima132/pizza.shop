e81fdcfefcf64bd8751bac74541254704e61393e
Leandro Ferreira <leandrolima_ferreira@hotmail.com>
2024-09-26 16:15:41 -0300

    feat: private router

--- src/pages/_layouts/app.tsx
+++ src/pages/_layouts/app.tsx
@@ -1,13 +1,43 @@
 import { Header } from "@/components/header";
-import { Outlet } from "react-router-dom";
+import { api } from "@/lib/axios";
+import { isAxiosError } from "axios";
+import { useLayoutEffect } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
 
 export function AppLayout() {
-  return(
+  const navigate = useNavigate();
+
+  useLayoutEffect(() => {
+    const interceptorId = api.interceptors.response.use(
+      (response) => response,
+      (error) => {
+        if (isAxiosError(error)) {
+          const status = error.response?.status;
+          const code = error.response?.data.code;
+
+          if (status === 401 && code === "UNAUTHORIZED") {
+            navigate("/sign-in", {
+              replace: true,
+            });
+          }
+        }
+
+        return Promise.reject(error);
+      }
+    );
+
+    // Clean up the side effect when the component unmounts
+    return () => {
+      api.interceptors.response.eject(interceptorId);
+    };
+  }, [navigate]);
+
+  return (
     <div className="flex min-h-screen flex-col antialiased">
-       <Header/>
+      <Header />
       <div className="flex flex-1 flex-col gap-4 p-8 pt-6">
         <Outlet />
       </div>
     </div>
-  )
+  );
 }
